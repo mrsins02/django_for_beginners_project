29696739d3c26e308a785d4af168f7cf0600f9e3
Amin Dehghan <amin.d.d.y1357@gmail.com>
2023-05-04 02:07:29 +0430

    home page test methods for url name added

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,5 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 
 class HomePageTests(SimpleTestCase):
@@ -6,3 +7,6 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home_page"))
+        self.assertEqual(response.status_code, 200)
