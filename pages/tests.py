dc7484c1b7e3783d65de7e95a32be70a9f70f883
Amin Dehghan <amin.d.d.y1357@gmail.com>
2023-05-04 02:41:56 +0430

    about us tests added

--- pages/tests.py
+++ pages/tests.py
@@ -10,3 +10,13 @@ class HomePageTests(SimpleTestCase):
     def test_url_available_by_name(self):
         response = self.client.get(reverse("home_page"))
         self.assertEqual(response.status_code, 200)
+
+
+class AboutUsPageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("about-us/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about_page"))
+        self.assertEqual(response.status_code, 200)
