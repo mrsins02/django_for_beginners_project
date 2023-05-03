f9ea30efad7f6b8020766c3a474b06738e97cf52
Amin Dehghan <amin.d.d.y1357@gmail.com>
2023-05-04 02:40:51 +0430

    about us url corrected

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,5 @@ from .views import HomeView, AboutUsView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home_page"),
-    path("about-us", AboutUsView.as_view(), name="about_page"),
+    path("about-us/", AboutUsView.as_view(), name="about_page"),
 ]
