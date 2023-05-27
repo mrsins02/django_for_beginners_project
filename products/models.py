7496dedd03283c1c2d3c5416f8d3b0625cfa201a
Amin Dehghan <amin.d.d.y1357@gmail.com>
2023-05-27 12:16:58 +0430

    typos from product's meta class ordering and delete page's url fixed; detail page's tests added.

--- products/models.py
+++ products/models.py
@@ -74,4 +74,4 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Product"
         verbose_name_plural = "Products"
-        ordering = ("date_created",)
+        ordering = ("-date_created",)
--- products/tests.py
+++ products/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import Product, Category, Color, Size, Brand
+from .models import Product, Category, Brand
 
 
 class ProductTests(TestCase):
@@ -8,9 +8,6 @@ class ProductTests(TestCase):
     def setUpTestData(cls):
         cls.category = Category.objects.create(category="test category")
         cls.brand = Brand.objects.create(brand="test brand")
-        cls.color = Color.objects.create(color_name="test color",
-                                         color_code="#111111")
-        cls.size = Size.objects.create(size="test size")
         cls.product = Product.objects.create(name="test product name",
                                              category_id=1,
                                              brand_id=1)
@@ -18,8 +15,6 @@ class ProductTests(TestCase):
     def test_slug(self):
         self.assertEqual(self.category.slug, "test-category")
         self.assertEqual(self.brand.slug, "test-brand")
-        self.assertEqual(self.color.slug, "test-color")
-        self.assertEqual(self.size.slug, "test-size")
         self.assertEqual(self.product.slug, "test-product-name")
 
     def test_product_list(self):
@@ -28,3 +23,14 @@ class ProductTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(reverse_response.status_code, 200)
         self.assertTemplateUsed(reverse_response, "products/product-list.html")
+
+    def test_product_detail(self):
+        response = self.client.get("/shop/products/test-product-name/")
+        reverse_response = self.client.get(reverse("product_detail", kwargs={"slug": self.product.slug}))
+
+        # for no response testing
+        no_response = self.client.get("/shop/products/test-product-name404/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(reverse_response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertTemplateUsed(reverse_response, "products/product-detail.html")
