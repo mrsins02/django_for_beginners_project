ba06f38d91971a7a1945d3d77026bc5aafe6986e
Amin Dehghan <amin.d.d.y1357@gmail.com>
2023-06-02 05:14:31 +0430

    Signup view and template modified; css for signup helptext added; login,logout,signup and dashboard urls added; shop and blog urls fixed; usercreationform modified for custom user.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
 
 user = get_user_model()
 
@@ -8,4 +10,5 @@ user = get_user_model()
 class SignUpView(CreateView):
     template_name = "accounts/sign-up.html"
     model = user
-    fields = "__all__"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
