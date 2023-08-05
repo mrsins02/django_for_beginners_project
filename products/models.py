fa943bb4f7ca3e3421abd47bdfe7cc101809df21
mrsins <amin.d.d.y1357@gmail.com>
2023-08-05 16:34:37 +0330

    product admin panel cofigured

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ import os.path
 from django.core import validators
 from django.db import models
 from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 
 
@@ -13,7 +14,7 @@ def get_ext(filename):
 
 def upload_dir(instance, filename):
     ext = get_ext(filename)
-    return f"media/products/{instance.product.category}/{instance.product.slug}/{instance.product.slug}.{ext}"
+    return f"products/{instance.product.category.slug[:32]}/{instance.product.slug}/{instance.product.slug}.{ext}"
 
 
 class Category(models.Model):
@@ -56,13 +57,27 @@ class Brand(models.Model):
         verbose_name_plural = "Brands"
 
 
-class ProductPicture(models.Model):
-    picture = models.ImageField(upload_to=upload_dir, unique=True)
-    is_main = models.BooleanField(default=False, verbose_name="Main Picture", help_text="Only one picture can be main!")
-    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+class Size(models.Model):
+    size = models.CharField(max_length=72, unique=True)
 
     def __str__(self):
-        return self.product.name.capitalize()
+        return self.size.capitalize()
+
+    class Meta:
+        verbose_name = "Size"
+        verbose_name_plural = "Sizes"
+
+
+class Color(models.Model):
+    color = models.CharField(max_length=72, unique=True)
+    code = models.CharField(max_length=72, unique=True)
+
+    def __str__(self):
+        return self.color.capitalize()
+
+    class Meta:
+        verbose_name = "Color"
+        verbose_name_plural = "Colors"
 
 
 class Product(models.Model):
@@ -74,11 +89,6 @@ class Product(models.Model):
     slug = models.SlugField(db_index=True, editable=False)
     category = models.ForeignKey(to="Category", on_delete=models.CASCADE)
     brand = models.ForeignKey(to="Brand", on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=12, decimal_places=2, default=0,
-                                validators=[validators.MinValueValidator(0)])
-    count = models.PositiveIntegerField(default=0)
-    sex = models.CharField(choices=sex_choices, default="male", max_length=16)
-    is_available = models.BooleanField(default=True)
     description = models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
@@ -98,3 +108,37 @@ class Product(models.Model):
         verbose_name = "Product"
         verbose_name_plural = "Products"
         ordering = ("-date_created",)
+
+
+class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    image = models.ImageField(blank=True, upload_to=upload_dir)
+
+    def __str__(self):
+        return f"{self.product.name}({self.pk})"
+
+    def image_tag(self):
+        if self.image.url is not None:
+            return mark_safe(f"<img src='{self.image.url}' height='50'/>")
+        else:
+            return ""
+    class Meta:
+        verbose_name = "Product Image"
+        verbose_name_plural = "Products Images"
+
+
+class Detail(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    count = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0,
+                                validators=[validators.MinValueValidator(0)])
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.product.name}({self.size})({self.color})"
+
+    class Meta:
+        verbose_name = "Product Detail"
+        verbose_name_plural = "Products Details"
--- products/views.py
+++ products/views.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponse
-from django.urls import reverse_lazy, reverse
-from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView, DetailView, TemplateView,CreateView, UpdateView, DeleteView, FormView
+
+from django.views.generic import ListView, DetailView
 
 from .models import Product
-from .forms import ProductCreateForm, ProductUpdateForm, ProductFormSet
 
 
 class ProductListView(ListView):
@@ -17,34 +14,4 @@ class ProductDetailView(DetailView):
     model = Product
 
 
-class DashboardView(ListView):
-    template_name = "products/dashboard.html"
-    model = Product
 
-
-class ProductCreateView(CreateView):
-    template_name = "products/product-create.html"
-    model = Product
-    form_class = ProductCreateForm
-
-
-class ProductUpdateView(UpdateView):
-    template_name = "products/product-update.html"
-    model = Product
-    form_class = ProductUpdateForm
-
-
-class ProductDeleteView(DeleteView):
-    template_name = "products/product-delete.html"
-    model = Product
-    success_url = reverse_lazy("product_dashboard")
-
-
-class PoductPictureUpdateView(UpdateView):
-    template_name = "products/picture-update.html"
-    model = Product
-    form_class = ProductFormSet
-
-    def get_success_url(self):
-        slug = self.get_object().slug
-        return reverse("product_detail", args=[slug, ])
